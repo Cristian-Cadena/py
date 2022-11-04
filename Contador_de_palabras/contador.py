7a1e7a3fc896947ca9f30e3a07aee127237cc927
CristianCadena <cristiancadena1927@gmail.com>
2022-11-03 21:52:03 -0500

    Proyecto Contador

---
+++ Contador_de_palabras/contador.py
@@ -0,0 +1,5 @@
+while True:
+    palabraUsuario = input('¿En que estas pensando? ')
+    contadorPalabras = len(palabraUsuario)
+    salidaUsuario = f'¡Muy bien, tu me has mostrado tu pensamiento en {contadorPalabras} palabras!'
+    print(salidaUsuario)
