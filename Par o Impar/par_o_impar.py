c9562e29863d403319f49be47594dee643f04d67
CristianCadena <cristiancadena1927@gmail.com>
2022-11-03 20:13:15 -0500

    Modificacion Final

--- Par o Impar/par_o_impar.py
+++ Par o Impar/par_o_impar.py
@@ -1,2 +1,15 @@
-x = int(input('Bienvenidos, por favor digita un numero del 1 - 1000'))
-print(x)
+while True:
+
+    x = int(input('Bienvenidos, por favor digita un numero del 1 - 1000: '))
+    y = x % 2
+
+    if x >= 1 and x <= 1000:
+        if y == 0:
+            print(f'El numero {x} es Par, presiona enter!!!\n')
+            
+        else:
+            print(f'El numero {x} es Impar\n')
+            
+    else:
+        print(f'Tu valor ingresado esta fuera del rango, digitalo nuevamente!!!\n')
+    
